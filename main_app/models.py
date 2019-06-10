c096fc2b6c9af8d6052e6abc1c575f458ac45a6a
Jessica Choi <jessica@Jessicas-MacBook-Air.local>
2019-06-10 16:09:38 -0700

    add models and details page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Plant(models.Model):
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    light = models.CharField(max_length=100, default='Medium indirect light')
+    water = models.CharField(max_length=100, default='When soil is dry')
+    humidity = models.CharField(max_length=100, default='Any humidity will do!')
+
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,5 @@
 from django.shortcuts import render
-
-class Plant:
-    def __init__(self, name, type, description, size):
-        self.name = name
-        self.type = type
-        self.description = description
-        self.size = size
-
-plants = [
-    Plant('Sir Teddington von Planten', 'Fiddle Leaf Fig', 'Loves the sun and weekly waterings', 'Medium'),
-    Plant('Mortimer', 'ZZ Plant', 'Likes to chill in the shade, water when dry', 'Medium'),
-    Plant('Moira', 'Echeveria', 'ALL the sun, water only when wrinkly', 'Mini'),
-]
-
+from .models import Plant
 
 # Create your views here.
 from django.http import HttpResponse
@@ -24,4 +11,9 @@ def about(request):
     return render(request, 'about.html')
 
 def plants_index(request):
+    plants = Plant.objects.all()
     return render(request, 'plants/index.html', { 'plants': plants })
+
+def plants_detail(request, plant_id):
+    plant = Plant.objects.get(id=plant_id)
+    return render(request, 'plants/detail.html', { 'plant': plant })
