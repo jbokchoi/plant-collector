33e77062a9720a51098f11db6c0e166327aee39d
Jessica Choi <jessica@Jessicas-MacBook-Air.local>
2019-06-12 10:04:49 -0700

    add feeding functionality

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+
+MEALS = (
+  ('W', 'Water'),
+  ('L', 'Light'),
+  ('F', 'Fertilizer')
+)
 
 # Create your models here.
 class Plant(models.Model):
@@ -11,3 +19,24 @@ class Plant(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'plant_id': self.id})
+
+    def fed_for_today(self):
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+
+class Feeding(models.Model):
+    date = models.DateField('feeding date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+    plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+
+    class Meta:
+        ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 from .models import Plant
+from .forms import FeedingForm
 
 # Create your views here.
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1> Hello Plants </h1')
+  return render(request, 'home.html')
 
 def about(request):
-    return render(request, 'about.html')
+  return render(request, 'about.html')
 
 def plants_index(request):
-    plants = Plant.objects.all()
-    return render(request, 'plants/index.html', { 'plants': plants })
+  plants = Plant.objects.all()
+  return render(request, 'plants/index.html', { 'plants': plants })
 
 def plants_detail(request, plant_id):
-    plant = Plant.objects.get(id=plant_id)
-    return render(request, 'plants/detail.html', { 'plant': plant })
+  plant = Plant.objects.get(id=plant_id)
+  feeding_form = FeedingForm()
+  return render(request, 'plants/detail.html', { 
+      'plant': plant, 'feeding_form': feeding_form 
+  })
+
+def add_feeding(request, plant_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.plant_id = plant_id
+    new_feeding.save()
+  return redirect('detail', plant_id=plant_id)
+
+class PlantCreate(CreateView):
+  model = Plant
+  fields = '__all__'
+  success_url = '/plants/'
+
+class PlantUpdate(UpdateView):
+  model = Plant
+  fields = ['species', 'description', 'light', 'water', 'humidity']
+
+class PlantDelete(DeleteView):
+  model = Plant
+  success_url = '/plants/'
